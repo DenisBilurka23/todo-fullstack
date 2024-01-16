77f6f63abd3e7fd7b64e486a5d526b9efc059232
DenisBilurka23 <vadya555555@gmail.com>
2024-01-16 16:09:16 -0500

    1.0.1

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { type Data } from '../types/types'
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export const getData: (task?: string) => Promise<Data[]> = async task => {
 	try {
-		const res = await axios.get(`http://localhost:8000/todo${task ? `/?task=${task}` : ''}`)
+		const res = await axios.get(`${API_URL}/todo${task ? `/?task=${task}` : ''}`)
 		return res.data
 	} catch (e) {
 		console.log(e)
@@ -12,7 +14,7 @@ export const getData: (task?: string) => Promise<Data[]> = async task => {
 
 export const addTodo: (task: string) => Promise<Data> = async task => {
 	try {
-		const res = await axios.post('http://localhost:8000/todo', { task })
+		const res = await axios.post(`${API_URL}/todo`, { task })
 		return res.data
 	} catch (e) {
 		console.log(e)
@@ -21,7 +23,7 @@ export const addTodo: (task: string) => Promise<Data> = async task => {
 
 export const deleteTodo: (id: number) => Promise<Data[]> = async id => {
 	try {
-		const res = await axios.delete(`http://localhost:8000/todo/${id}`)
+		const res = await axios.delete(`${API_URL}/todo/${id}`)
 		return res.data
 	} catch (e) {
 		console.log(e)
@@ -33,7 +35,7 @@ export const updateTodo: (id: number, payload: { task?: string; active?: boolean
 	payload
 ) => {
 	try {
-		const res = await axios.patch(`http://localhost:8000/todo/${id}`, payload)
+		const res = await axios.patch(`${API_URL}/todo/${id}`, payload)
 		return res.data
 	} catch (e) {
 		console.log(e)
